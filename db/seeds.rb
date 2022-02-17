b2de2900f0f19bb5ca6971a5d188f322b7f1b65f
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-02-17 22:00:35 +0000

    added movies using api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
 
 puts 'creating movies'
 
-Movie.create!(title: 'Wonder Woman 1984', overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s', poster_url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', rating: 6.9)
-Movie.create!(title: 'The Shawshank Redemption', overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison', poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg', rating: 8.7)
-Movie.create!(title: 'Titanic', overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9)
-Movie.create!(title: 'Ocean\'s Eight', overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.', poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg', rating: 7.0)
+url = 'http://tmdb.lewagon.com/movie/upcoming'
+
+buffer = URI.open(url).read
+
+result = JSON.parse(buffer)
+
+result['results'].each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    rating: movie['vote_average.floor'],
+    poster_url: movie["https://image.tmdb.org/t/p/w500#{movie['poster_path']}"]
+  ) unless Movie.where('title = ?', movie['title']).size > 0
+end
 
 puts 'created movies'
