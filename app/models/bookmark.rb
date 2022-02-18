15b46707261395f1a024d218e37478d738790e93
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-02-18 10:46:27 +0000

    stuff

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }
-  validates_uniqueness_of :movie, :list, scope: %i[movie_id list_id]
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
